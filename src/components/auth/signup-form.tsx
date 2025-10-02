d7c8b502e22e46f770a756884dd5975ae8c5bb11
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-10-02 15:14:07 +0200

    feat: signup and login links

--- src/components/auth/signup-form.tsx
+++ src/components/auth/signup-form.tsx
@@ -74,7 +74,7 @@ export function SignupForm({
             </div>
             <div className="mt-4 text-center text-sm">
               Already have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
+              <a href="/auth/login" className="underline underline-offset-4">
                 Log in
               </a>
             </div>
