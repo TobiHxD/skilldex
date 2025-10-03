80386878a7dcd815c282341aa6f453384c59b1f9
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-10-03 13:58:34 +0200

    feat: avatar fallback to initial

--- src/components/sidebar/account.tsx
+++ src/components/sidebar/account.tsx
@@ -1,30 +1,24 @@
 import { authClient } from "@/lib/auth-client"
 import { ChevronRight } from "lucide-react"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 export default function Account() {
     const { data: session } = authClient.useSession()
 
-    const getProfileImage = () => {
-        if (session?.user.image) {
-            return (
-                <img
-                    src={session.user.image}
-                    alt="Profile"
-                    className="h-10 w-10 rounded-xl"
-                />
-            )
-        } else {
-            return (
-                <div className="h-10 w-10 rounded-xl bg-muted" />
-            )
-        }
+    const ProfileImage = () => {
+        return (
+            <Avatar className="rounded-lg h-10 w-10">
+                <AvatarImage src={session?.user.image || undefined} />
+                <AvatarFallback className="rounded-lg h-10 w-10">{session?.user.name?.charAt(0)}</AvatarFallback>
+            </Avatar>
+        )
     }
 
     return (
         <div className="p-2">
-            <div className="flex items-center space-x-3 hover:bg-accent hover:rounded-2xl p-2">
+            <div className="flex items-center space-x-3 p-2 hover:bg-accent hover:rounded-2xl hover:cursor-pointer">
                 <div className="aspect-square flex items-center">
-                    {getProfileImage()}
+                    <ProfileImage />
                 </div>
                 <div className="flex flex-col">
                     <h3>{session?.user.name}</h3>
