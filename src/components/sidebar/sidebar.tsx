35ee7c1006b213eb8ba43d64caee18c03a9a0fa6
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-10-03 15:14:42 +0200

    fix: show trigger in the sidebar on mobile

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -2,13 +2,18 @@ import {
     Sidebar,
     SidebarContent,
     SidebarFooter,
+    SidebarTrigger,
 } from "@/components/ui/sidebar";
 import Account from "./account";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export default function AppSidebar() {
+    const isMobile = useIsMobile();
+
     return (
         <Sidebar>
             <SidebarContent>
+                {isMobile && <SidebarTrigger />}
             </SidebarContent>
             <SidebarFooter>
                 <Account />
