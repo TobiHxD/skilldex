ac9b955ab5b58fc2fc99bb0f181e0e6a0b2f17f4
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-10-12 17:55:06 +0200

    chore: add a nix flake for development environment

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Skilldex";
 
   inputs = {
-    nixpkgs = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
   outputs = { nixpkgs, ... }: 
@@ -21,11 +21,24 @@
           pkgs = import nixpkgs { inherit system; };
         in
         {
-          buildInputs = [
-            pkgs.nodejs_24
-            pkgs.bun
-            pkgs.pnpm
-          ];
-      });
-    };
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.nodejs_24
+              pkgs.bun
+              pkgs.pnpm
+
+              pkgs.nodePackages.prisma
+            ];
+
+            shellHook = ''
+              export PRISMA_MIGRATION_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+              export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
+              export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
+              export PRISMA_INTROSPECTION_ENGINE_BINARY="${pkgs.prisma-engines}/bin/introspection-engine"
+              export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
+            '';
+          };
+        }
+    );
+  };
 }
