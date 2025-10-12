3be4b4226f98e3d45f18056d6dd45c56175cb01e
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-10-12 19:17:16 +0200

    feat: add dropzone for profile picture upload

--- src/app/app/settings/profile-picture-update.tsx
+++ src/app/app/settings/profile-picture-update.tsx
@@ -1,12 +1,22 @@
-import { DialogContent } from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button"
+import { DialogContent, DialogFooter, DialogHeader } from "@/components/ui/dialog"
 import Dropzone from "@/components/ui/dropzone"
+import { authClient } from "@/lib/auth-client";
 import { DialogTitle } from "@radix-ui/react-dialog"
 
 export default function ProfilePictureUpdate() {
-    return (
-        <DialogContent>
-            <DialogTitle>Update Profile Picture</DialogTitle>
-            <Dropzone />
-        </DialogContent>
-    )
+  const handleUpload = () => {
+  };
+
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Update Profile Picture</DialogTitle>
+      </DialogHeader>
+      <Dropzone />
+      <DialogFooter>
+        <Button type="submit">Upload</Button>
+      </DialogFooter>
+    </DialogContent>
+  )
 }
--- src/components/ui/dropzone.tsx
+++ src/components/ui/dropzone.tsx
@@ -1,17 +1,48 @@
 "use client"
 
 import { useState } from "react"
+import { Button } from "./button";
+
+interface DropzoneProps {
+  file: File | null
+}
+
+function DropzoneContent({ file }: DropzoneProps) {
+  if (file) {
+    return (
+      <div className="flex flex-col">
+        <span>{file.name}</span>
+        <Button variant="ghost" className="mt-2 text-destructive">Remove</Button>
+      </div>
+    )
+  } else {
+    return (
+      <span>Click or Drag and Drop to upload</span>
+    )
+  }
+}
 
 export default function Dropzone() {
-    const [file, setFile] = useState<File | null>(null);
+  const [file, setFile] = useState<File | null>(null);
 
-    return (
-        <div 
-            className="hover:bg-muted/20 w-full h-28 border-2 border-dashed border-muted rounded-lg flex items-center justify-center text-muted-foreground"
-        >
-            <input id="file" type="file" className="opacity-0 cursor-pointer" />
-            <span>Click or Drag and Drop to upload</span>
+  const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setFile(e.target.files[0]);
+    }
+  };
 
-        </div>
-    )
+  return (
+      <div 
+          className="relative hover:bg-muted/20 w-full h-28 border-2 border-dashed border-muted rounded-lg flex items-center justify-center text-muted-foreground"
+      >
+          <input 
+            id="file" 
+            type="file"
+            className="absolute w-full h-full opacity-0 cursor-pointer"
+            onChange={handleUpload} 
+            hidden={file != null}
+          />
+          <DropzoneContent file={file} />
+      </div>
+  )
 }
