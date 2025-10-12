235f03616bd0ae854acd3f0b0681d7016218f985
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-10-12 18:47:54 +0200

    fix(flake): switch to PRISMA_SCHEMA_ENGINE_BINARY

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
               # TODO: Find a better solution
               export PRISMA_DISABLE_WARNINGS=1
 
-              export PRISMA_MIGRATION_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+              export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
               export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
               export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
               export PRISMA_INTROSPECTION_ENGINE_BINARY="${pkgs.prisma-engines}/bin/introspection-engine"
