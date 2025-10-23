9380811de8a26e3453345ff091f902241989d53d
Nicolas Hartmann <nicolas.hartmann@ptb.de>
2025-10-23 12:29:43 +0200

    wip: add profile picture upload

--- src/app/app/settings/profile-picture-update.tsx
+++ src/app/app/settings/profile-picture-update.tsx
@@ -1,21 +1,31 @@
-import { Button } from "@/components/ui/button"
-import { DialogContent, DialogFooter, DialogHeader } from "@/components/ui/dialog"
-import Dropzone from "@/components/ui/dropzone"
+"use client";
+
+import { DialogContent, DialogHeader } from "@/components/ui/dialog"
+// import Dropzone from "@/components/ui/dropzone"
+import { generateUploadDropzone } from "@uploadthing/react"
+import { profilePictureFileRouter } from "@/app/api/uploadthing/profile_picture"
 import { DialogTitle } from "@radix-ui/react-dialog"
+import { toast } from "sonner";
+import { authClient } from "@/lib/auth-client";
 
 export default function ProfilePictureUpdate() {
-  const handleUpload = () => {
-  };
+  const UploadDropzone = generateUploadDropzone<profilePictureFileRouter>();
 
   return (
     <DialogContent>
       <DialogHeader>
         <DialogTitle>Update Profile Picture</DialogTitle>
       </DialogHeader>
-      <Dropzone />
-      <DialogFooter>
-        <Button type="submit">Update</Button>
-      </DialogFooter>
+      <UploadDropzone
+        endpoint="profilePicture"
+        onClientUploadComplete={(res) => {
+          authClient.updateUser
+          toast.success("Profile picture uploaded successfully!");
+        }}
+        onUploadError={(error: Error) => {
+          toast.error(`Upload failed: ${error.message}`);
+        }}
+      />
     </DialogContent>
   )
 }
