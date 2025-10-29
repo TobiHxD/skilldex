4a4b5e55c23245afd15fe88b909a88c92e39e16d
Nicolas Hartmann <nicolas.hartmann@ptb.de>
2025-10-29 17:18:11 +0100

    wip: bottom banner

---
+++ src/components/banner.tsx
@@ -0,0 +1,7 @@
+export function Banner() {
+  return (
+    <div className="bg-blue-600 text-white py-2 px-4 text-center absolute bottom-0 inset-x-0">
+      Welcome to Skilldex! Enhance your skills and track your progress.
+    </div>
+  );
+}
