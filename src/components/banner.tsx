46b56c99d6372d036156ee5377ca166b82a7cacb
Nicolas Hartmann <nicolas.hartmann@ptb.de>
2025-10-30 08:30:58 +0100

    feat: add email unverified banner

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { Button } from "@/components/ui/button";
+
 export function Banner() {
-  return (
-    <div className="bg-blue-600 text-white py-2 px-4 text-center absolute bottom-0 inset-x-0">
-      Welcome to Skilldex! Enhance your skills and track your progress.
-    </div>
-  );
+  const { data: session } = authClient.useSession();
+
+  if (session?.user.emailVerified === false) {
+    return (
+      <div className="bg-blue-500 text-white py-2 px-4 text-center absolute bottom-0 inset-x-0 flex justify-center items-center">
+          <span className="font-semibold text-sm">Please verify your email address to access all features.</span>
+          <Button variant="ghost" size="sm" className="ml-2 h-7">Resend</Button>
+          <Button variant="ghost" size="sm" className="h-7">Close</Button>
+      </div>
+    );
+  };
 }
